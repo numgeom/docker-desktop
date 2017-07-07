e20b31968def62d43e46806825cba44d1c606a3d
Xiangmin Jiao <xmjiao@gmail.com>
2017-07-07 19:02:47 -0400

    Chagned startup script

--- image/home/.numgeom/startup.m
+++ image/home/.numgeom/startup.m
@@ -1,4 +1,4 @@
-run $DOCKER_HOME/fastsolve/ilupack4m/startup.m
-run $DOCKER_HOME/fastsolve/paracoder/startup.m
-run $DOCKER_HOME/fastsolve/petsc4m/startup.m
-run $DOCKER_HOME/numgeom2/ahf-plus/matlab/startup.m
+run ~/fastsolve/ilupack4m/startup.m
+run ~/fastsolve/paracoder/startup.m
+run ~/fastsolve/petsc4m/startup.m
+run ~/numgeom2/ahf-plus/matlab/startup.m
