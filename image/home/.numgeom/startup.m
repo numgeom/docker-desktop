7a4ab4b969024d484f27f205046e844981084377
Xiangmin Jiao <xmjiao@gmail.com>
2017-07-07 23:56:33 -0400

    Changed home directory

--- image/home/.numgeom/startup.m
+++ image/home/.numgeom/startup.m
@@ -1,4 +1,4 @@
-run ~/fastsolve/ilupack4m/startup.m
-run ~/fastsolve/paracoder/startup.m
-run ~/fastsolve/petsc4m/startup.m
-run ~/numgeom2/ahf-plus/matlab/startup.m
+run /home/ubuntu/fastsolve/ilupack4m/startup.m
+run /home/ubuntu/fastsolve/paracoder/startup.m
+run /home/ubuntu/fastsolve/petsc4m/startup.m
+run /home/ubuntu/numgeom2/ahf-plus/matlab/startup.m
