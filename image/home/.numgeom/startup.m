ddfa52a201376c5ce26e08632fcc25b07af28f9d
Xiangmin Jiao <xmjiao@gmail.com>
2019-09-22 00:24:24 -0400

    updated project path

--- image/home/.numgeom/startup.m
+++ image/home/.numgeom/startup.m
@@ -1,9 +1,9 @@
-run ~/fastsolve/ilupack4m/startup.m
-run ~/fastsolve/paracoder/startup.m
-run ~/fastsolve/petsc4m/startup.m
-run ~/numgeom2/ahf-plus/matlab/startup.m
+run ~/project/fastsolve/ilupack4m/startup.m
+run ~/project/fastsolve/paracoder/startup.m
+run ~/project/fastsolve/petsc4m/startup.m
+run ~/project/numgeom2/ahf-plus/matlab/startup.m
 
 if ~exist('OCTAVE_VERSION', 'builtin')
     % Set up MBeautifier in MATLAB
-    addpath('~/numgeom2/MBeautifier')
+    addpath('~/project/numgeom2/MBeautifier')
 end
